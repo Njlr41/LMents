d7861a9d9b3357e8337acdd90ee88a9715b20b9e
Njlr41 <nicholasjohnreyes@gmail.com>
2025-02-10 09:04:51 +0800

    Start working on a better UI. Followed Google Sign-in Button Branding Guidelines.

--- LMents-app/src/routes/+layout.svelte
+++ LMents-app/src/routes/+layout.svelte
@@ -1,20 +1,49 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/signup">Login</a>
-    <a href="/courses">Courses</a>
-    <a href="/assignments">Assignments</a>
-    <a href="/announcements">Announcements</a>
-</nav>
+<script>
+
+    import { goto } from "$app/navigation";
+
+</script>
+<div class = "head">
+    <button on:click={()=>{goto('/')}}>Home</button>
+    <button on:click={()=>{goto('/signup')}}>Login</button>
+    <button on:click={()=>{goto('/courses')}}>Courses</button>
+    <button on:click={()=>{goto('/assignments')}}>Assignments</button>
+    <button on:click={()=>{goto('/announcements')}}>Announcements</button>
+</div>
 
 <main>
     <slot/>
 </main>
   
 <style>
-    nav {
+    main {
+        flex: 1;
+        padding: 5rem;
+    }
+
+    .head {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+        gap: 15px;
         justify-content: center;
         align-items: center;
-        }
+    }
+    
+    button {
+        padding: 10px;
+        font-size: 20px;
+        border: 0;
+        border-radius: 100px;
+        box-shadow: 2px 3px 4px #333;
+        background-color: #b5b5b5;
+    }
+    
+    button:hover {
+        padding: 10px;
+        font-size: 20px;
+        border: 0;
+        border-radius: 100px;
+        box-shadow: 2px 3px 4px #333;
+        background-color: #606060;
+    }
 </style>
