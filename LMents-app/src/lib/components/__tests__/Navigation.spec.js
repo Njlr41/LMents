e97c48179e552d3d32c1ed30a69d14a669093aee
Njlr41 <nicholasjohnreyes@gmail.com>
2025-02-11 22:18:53 +0800

    Added more Tests

--- LMents-app/src/lib/components/__tests__/Navigation.spec.js
+++ LMents-app/src/lib/components/__tests__/Navigation.spec.js
@@ -8,4 +8,28 @@ describe('Navigation', () => {
     const Home = screen.getByText('Home');
     expect(Home).toBeTruthy();
   });
+
+  test('Login Button Renders', () => {
+    render(Navigation);
+    const Login = screen.getByText('Login');
+    expect(Login).toBeTruthy();
+  })
+
+  test('Courses Button Renders', () => {
+    render(Navigation);
+    const Courses = screen.getByText('Courses');
+    expect(Courses).toBeTruthy();
+  })
+
+  test('Assignments Button Renders', () => {
+    render(Navigation);
+    const Assignments = screen.getByText('Assignments');
+    expect(Assignments).toBeTruthy();
+  })
+
+  test('Announcements Button Renders', () => {
+    render(Navigation);
+    const Announcements = screen.getByText('Announcements');
+    expect(Announcements).toBeTruthy();
+  })
 })
