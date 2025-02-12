a715195719f41fc886f7e29cbf0b44acc3b10605
Njlr41 <nicholasjohnreyes@gmail.com>
2025-02-12 18:41:26 +0800

    Test

--- LMents-app/src/routes/assignments/+page.svelte
+++ LMents-app/src/routes/assignments/+page.svelte
@@ -25,7 +25,7 @@
                     {#if course_work.dueDate}
                         <p>{course_work.dueDate.year}, {intToMonth(course_work.dueDate.month)} {course_work.dueDate.day}</p>
                     {:else}
-                        <p>No deadline.</p>
+                        <p>No deadline. TEST</p>
                     {/if}
                 </div>
             {/each}
