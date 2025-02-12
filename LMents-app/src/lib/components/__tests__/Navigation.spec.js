b9f8b4842c385eb9f3b3283c9dad18975b36c80a
Njlr41 <nicholasjohnreyes@gmail.com>
2025-02-12 14:49:08 +0800

    Completed Tests for Sprint 1

--- LMents-app/src/lib/components/__tests__/Navigation.spec.js
+++ LMents-app/src/lib/components/__tests__/Navigation.spec.js
@@ -1,35 +1,49 @@
 // src/lib/components/__tests__/Navigation.spec.js
-import { render, screen } from '@testing-library/svelte';
-import Navigation from '../Navigation.svelte'; // import the .svelte component
+import { render, screen, fireEvent } from '@testing-library/svelte';
+import Navigation from '../Navigation.svelte';
+import { goto } from '$app/navigation';
+
+vi.mock('$app/navigation');
 
 describe('Navigation', () => {
-  test('Home Button Renders', () => {
+  test('Home Button is Functional', () => {
     render(Navigation);
     const Home = screen.getByText('Home');
     expect(Home).toBeTruthy();
+    fireEvent.click(Home);
+    expect(goto).toHaveBeenCalledWith('/');
   });
-
-  test('Login Button Renders', () => {
+  
+  test('Login Button is Functional', () => {
     render(Navigation);
     const Login = screen.getByText('Login');
     expect(Login).toBeTruthy();
-  })
+    fireEvent.click(Login);
+    expect(goto).toHaveBeenCalledWith('/signup');
+  });
 
-  test('Courses Button Renders', () => {
+  test('Courses Button is Functional', () => {
     render(Navigation);
     const Courses = screen.getByText('Courses');
     expect(Courses).toBeTruthy();
-  })
+    fireEvent.click(Courses);
+    expect(goto).toHaveBeenCalledWith('/courses');
+  });
 
-  test('Assignments Button Renders', () => {
+  test('Assignments Button is Functional', () => {
     render(Navigation);
     const Assignments = screen.getByText('Assignments');
     expect(Assignments).toBeTruthy();
-  })
+    fireEvent.click(Assignments);
+    expect(goto).toHaveBeenCalledWith('/assignments');
+  });
 
-  test('Announcements Button Renders', () => {
+  test('Announcements Button is Functional', () => {
     render(Navigation);
     const Announcements = screen.getByText('Announcements');
     expect(Announcements).toBeTruthy();
-  })
+    fireEvent.click(Announcements);
+    expect(goto).toHaveBeenCalledWith('/announcements');
+  });
 })
+
