f696f9cf4168f711a28a66887320e758cd560523
Njlr41 <nicholasjohnreyes@gmail.com>
2025-02-26 19:58:46 +0800

    Moved Mobile Login button

--- LMents-app/src/routes/signup/+page.server.js
+++ LMents-app/src/routes/signup/+page.server.js
@@ -2,27 +2,43 @@ import { redirect } from '@sveltejs/kit';
 import { OAuth2Client } from 'google-auth-library';
 import {AUTH_GOOGLE_ID,AUTH_GOOGLE_SECRET} from '$env/static/private';
 import { SocialLogin } from "@capgo/capacitor-social-login";
+import { json } from '@sveltejs/kit';
 
 export const actions = {
-    OAuth2: async({})=>{
-        const redirectURL = 'http://localhost:5173/oauth';
+  OAuth2: async({})=>{
+      const redirectURL = 'http://localhost:5173/oauth';
 
-        const oAuth2Client = new OAuth2Client(
-            AUTH_GOOGLE_ID,
-            AUTH_GOOGLE_SECRET,
-            redirectURL
-          );
-      
-          // Generate the url that will be used for the consent dialog.
-          const authorizeUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: ['https://www.googleapis.com/auth/classroom.courses.readonly',
-                    'https://www.googleapis.com/auth/classroom.course-work.readonly',
-                    'https://www.googleapis.com/auth/classroom.announcements.readonly'],
-            prompt: 'consent'
-          });
+      const oAuth2Client = new OAuth2Client(
+          AUTH_GOOGLE_ID,
+          AUTH_GOOGLE_SECRET,
+          redirectURL
+        );
+    
+        // Generate the url that will be used for the consent dialog.
+        const authorizeUrl = oAuth2Client.generateAuthUrl({
+          access_type: 'offline',
+          scope: ['https://www.googleapis.com/auth/classroom.courses.readonly',
+                  'https://www.googleapis.com/auth/classroom.course-work.readonly',
+                  'https://www.googleapis.com/auth/classroom.announcements.readonly'],
+          prompt: 'consent'
+        });
 
-          throw redirect(302,authorizeUrl);
+        throw redirect(302,authorizeUrl);
+  },
+  // Store access token in cookies
+  mobileLogin: async ({ cookies, request }) => {
+    const formData = await request.formData();
+    const accessToken = formData.get('accessToken');
+
+    cookies.set('access_token', accessToken, {
+        httpOnly: true,
+        secure: true,  
+        path: '/',
+        sameSite: 'strict',
+        maxAge: 60 * 60
+    });
+
+    return({ success: true });
     }
 }
 
