5a03fe4afab5ea77727cba46a3ab3141a13d22dd
Njlr41 <nicholasjohnreyes@gmail.com>
2025-05-07 18:26:12 +0800

    feat: fix theme.js

--- LMents-app/src/lib/theme.js
+++ LMents-app/src/lib/theme.js
@@ -5,20 +5,22 @@ export const theme_color = writable('#F2B344');
 export const theme_color_name = writable('Yellow');
 
 if (typeof window !== 'undefined'){
-    let color = await Preferences.get({ key: 'theme_color' })
-    if (color){
-        theme_color.set(color.value);
-    } 
-    theme_color.subscribe((color) => {
-        Preferences.set({ key: 'theme_color', value: color });
-    });
-
-    let color_name = await Preferences.get({ key: 'theme_color_name' })
-    if (color_name){
-        theme_color_name.set(color_name.value);
-    } 
-    theme_color_name.subscribe((color_name) => {
-        Preferences.set({ key: 'theme_color_name', value: color_name });
-    });
+    (async () => {
+        let color = await Preferences.get({ key: 'theme_color' })
+        if (color){
+            theme_color.set(color.value);
+        } 
+        theme_color.subscribe((color) => {
+            Preferences.set({ key: 'theme_color', value: color });
+        });
+        
+        let color_name = await Preferences.get({ key: 'theme_color_name' })
+        if (color_name){
+            theme_color_name.set(color_name.value);
+        } 
+        theme_color_name.subscribe((color_name) => {
+            Preferences.set({ key: 'theme_color_name', value: color_name });
+        });
+    })
 }
 
